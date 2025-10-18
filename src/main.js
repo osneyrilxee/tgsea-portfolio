771d639bc47577b1bb19291c9ffd3547e5726a4d
Rilxee <117995295+osneyrilxee@users.noreply.github.com>
2025-10-18 21:46:06 +0200

    upload some comment javascript for any language not loading rn

--- src/main.js
+++ src/main.js
@@ -5,3 +5,23 @@ logoNavigation.addEventListener("click", function () {
   sidebar.classList.toggle("active");
   logoNavigation.classList.toggle("active");
 })
+
+// const translations = {
+//   fr: { select: "Sélectionner une langue" },
+//   en: { select: "Select a language" }
+// };
+
+// const select = document.getElementById("selectLanguage");
+
+// const setLanguage = (language) => {
+//   if (language === "fr") {
+//     select.innerText = translations.fr.select;
+//   } else if (language === "en") {
+//     select.innerText = translations.en.select;
+//   }
+// };
+
+// const languageSelectTop = document.querySelector("select");
+// languageSelectTop.addEventListener("change", (event) => {
+//   setLanguage(event.target.value);
+// });
