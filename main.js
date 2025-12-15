8b8136ba79c9df5eb89a966edf0605bcde9b77b5
Rilxee <117995295+osneyrilxee@users.noreply.github.com>
2025-12-15 19:44:55 +0100

    hide nav bar for tablet and phone + add feature but doesn't work yet because I need learn js

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// const logoNavigation = document.getElementById("navigation-logo-side-bar-event");
-// const sidebar = document.querySelector(".div__side-bar--active");
-
-// logoNavigation.addEventListener("click", function () {
-//   sidebar.classList.toggle("active");
-//   logoNavigation.classList.toggle("active");
-// })
-
-// const translations = {
-//   fr: { select: "Sélectionner une langue" },
-//   en: { select: "Select a language" }
-// };
-
-// const select = document.getElementById("selectLanguage");
-
-// const setLanguage = (language) => {
-//   if (language === "fr") {
-//     select.innerText = translations.fr.select;
-//   } else if (language === "en") {
-//     select.innerText = translations.en.select;
-//   }
-// };
-
-// const languageSelectTop = document.querySelector("select");
-// languageSelectTop.addEventListener("change", (event) => {
-//   setLanguage(event.target.value);
-// });
+// function dropDown() {
+//     click = document.getElementById("kr").addEventListener("click", displatDate);
+// }
