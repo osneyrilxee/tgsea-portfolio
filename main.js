bbd45727ef7e59a7a0624d7438bad042cab2660f
rilxee <rilxee.contact.pro@gmail.com>
2026-05-24 23:44:00 +0000

    add new branch hambuger-menu for testing something not finish

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // function dropDown() {
 //     click = document.getElementById("kr").addEventListener("click", displatDate);
 // }
+
+console.log("JS chargé");
+
+var select = document.querySelector(".hamburger");
+
+select.addEventListener('click', function() {
+    document.querySelector(".nav-bar").classList.toggle("show");
+})
